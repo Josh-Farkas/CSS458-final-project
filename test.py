0c80dbbe06068e850b77d86dcbdeed2d0068ff79
Kyle <kylewilliams342@gmail.com>
2025-11-28 13:12:41 -0800

    minor changes

--- test.py
+++ test.py
@@ -76,7 +76,7 @@ def animation_test():
         time_steps_set.append(set_copy)
 
     animation = ani.Animation(time_steps_set)
-    animation.centered_sun()
+    print(time_steps_set)
     
 
 animation_test()
