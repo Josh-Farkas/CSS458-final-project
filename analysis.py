3fdc3a8dfc5da777892d2dd0797c9cad8a814421
Farkler <59152302+Farkler0@users.noreply.github.com>
2025-12-03 15:43:29 -0800

    Add relative error to analysis

--- analysis.py
+++ analysis.py
@@ -85,6 +85,19 @@ class Analysis:
 
    #========================================Verification Methods-Physcis related=====================================         
 
+    def relative_error(self, v1, v2):
+        """Calculates the relative error of two values
+
+        Args:
+            v1 (float): True value
+            v2 (float): Tested value
+
+        Returns:
+            float: The relative error between values as a percentage
+        """
+        return (v1 - v2) / v1
+
+
     def calculate_total_energy(self, bodies):
         """
         Calculates the sum of kinetic energy  and potential energy for all bodies
@@ -131,7 +144,7 @@ class Analysis:
         for time_step in history:
             bodies = time_step
             energy.append(self.calculate_total_energy(bodies))
-
+        
         return energy
 
 
@@ -189,7 +202,10 @@ class Analysis:
         energy = self.check_conservation_of_energy(run_name)
         length = len(energy)
         dt = self.runs[run_name]["dt"]
-        time_step = np.array(range(length)) * dt 
+        time_step = np.array(range(length)) * dt
+        
+        err = self.relative_error(energy[0], energy[-1])
+        print(f'Energy Relative Error: {err}')
 
         plt.plot(time_step, energy)
         plt.title("Conservation of Energy")
@@ -208,12 +224,14 @@ class Analysis:
         time_step = np.array(range(length)) * dt
         magnitudes = [np.linalg.norm(m) for m in momentum]
 
+        err = self.relative_error(magnitudes[0], magnitudes[-1])
+        print(f'Momentum Relative Error: {err}')
+
         plt.plot(time_step, magnitudes)
         plt.title("Conservation of Momentum")
         plt.xlabel("Time(seconds)")
         plt.ylabel("Momentum Magnitude")
         plt.show()
-        pass
 
 
     def plot_success_metrics(self, run_name):
